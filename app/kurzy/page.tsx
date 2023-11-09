0d8534ce7e3b435850bdc2694cc5265ec2aef834
David <davidkojecky@gmail.com>
2023-11-09 17:20:47 +0100

    transitions

---
+++ app/kurzy/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+export default async function courses() {
+    return <main>
+        <div>
+            <Link href="/">
+                Home
+            </Link>
+        </div>
+    </main>
+}
